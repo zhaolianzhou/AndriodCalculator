13f5a0b24e92df47714e6e7443e5c6e759b8d0ff
Pubudu  Dissanayake <u5857495@anu.edu.au>
2016-05-08 22:30:43 +1000

    Added rest of the operations in MainActivity

--- app/src/main/java/com/vera/zzl/comp6442_assignment_two_2016/MainActivity.java
+++ app/src/main/java/com/vera/zzl/comp6442_assignment_two_2016/MainActivity.java
@@ -11,131 +11,302 @@ import com.vera.zzl.comp6442_assignment_two_2016.core.Expressions;
 import com.vera.zzl.comp6442_assignment_two_2016.core.Tokenizer;
 import com.vera.zzl.comp6442_assignment_two_2016.exceptions.ParseException;
 
+/**
+ * MainActivity
+ */
 public class MainActivity extends AppCompatActivity {
     int plusOrMinusCounter = 0;
     TextView textInputView;
     TextView textOutputView;
     String displayValue = "";
-
     boolean equalClicked = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textInputView =(TextView) findViewById(R.id.textViewDisplay);
+        textInputView = (TextView) findViewById(R.id.textViewDisplay);
         textOutputView = (TextView) findViewById(R.id.textOutputView);
-        Typeface type = Typeface.createFromAsset(getAssets(),"fonts/digital-7.ttf");
+        Typeface type = Typeface.createFromAsset(getAssets(), "fonts/digital-7.ttf");
         textInputView.setTypeface(type);
         textOutputView.setTypeface(type);
 
     }
 
-    public  void clearData(View view){
+    /**
+     * @param view
+     */
+    public void clearData(View view) {
         displayValue = "";
         textInputView.setText("");
         textOutputView.setText("");
     }
 
-    private void updateTextView(String value){
-        displayValue = displayValue + value ;
+    /**
+     * @param value
+     */
+    private void updateTextView(String value) {
+        displayValue = displayValue + value;
         textInputView.setText(displayValue);
     }
-    public void buttonValueSeven(View view){
+
+    /**
+     * @param view
+     */
+    public void buttonValueSeven(View view) {
 
         startNewExpression(view);
         updateTextView("7");
     }
 
+    /**
+     * @param view
+     */
     private void startNewExpression(View view) {
         if (equalClicked) {
-            equalClicked =false;
+            equalClicked = false;
             clearData(view);
         }
     }
 
-    public void buttonValueEight(View view){
+    /**
+     * @param view
+     */
+    public void buttonValueEight(View view) {
         startNewExpression(view);
         updateTextView("8");
     }
-    public void buttonValueNine(View view){
+
+    /**
+     * @param view
+     */
+    public void buttonValueNine(View view) {
         startNewExpression(view);
         updateTextView("9");
     }
-    public void buttonValueFour(View view){
+
+    /**
+     * @param view
+     */
+    public void buttonValueFour(View view) {
         startNewExpression(view);
         updateTextView("4");
     }
-    public void buttonValueFive(View view){
+
+    /**
+     * @param view
+     */
+    public void buttonValueFive(View view) {
         startNewExpression(view);
         updateTextView("5");
     }
-    public void buttonValueSix(View view){
+
+    /**
+     * @param view
+     */
+    public void buttonValueSix(View view) {
         startNewExpression(view);
         updateTextView("6");
     }
-    public void buttonValueOne(View view){
+
+    /**
+     * @param view
+     */
+    public void buttonValueOne(View view) {
         startNewExpression(view);
         updateTextView("1");
     }
-    public void buttonValueTwo(View view){
+
+    /**
+     * @param view
+     */
+    public void buttonValueTwo(View view) {
         startNewExpression(view);
         updateTextView("2");
     }
-    public void buttonValueThree(View view){
+
+    /**
+     * @param view
+     */
+    public void buttonValueThree(View view) {
         startNewExpression(view);
         updateTextView("3");
     }
-    public void buttonValueZero(View view){
+
+    /**
+     * @param view
+     */
+    public void buttonValueZero(View view) {
         startNewExpression(view);
         updateTextView("0");
     }
-    public void buttonValueDot(View view){
 
+    /**
+     * @param view
+     */
+    public void buttonValueDot(View view) {
         startNewExpression(view);
         updateTextView(".");
     }
 
-    public void buttonOperatorInc(View view){
+    /**
+     * @param view
+     */
+    public void buttonOperatorInc(View view) {
         startNewExpression(view);
-        updateTextView("+");}
-    public void buttonOperatorDec(View view){
+        updateTextView("+");
+    }
+
+    /**
+     * @param view
+     */
+    public void buttonOperatorDec(View view) {
         startNewExpression(view);
-        updateTextView("-");}
-    public void buttonOperatorMul(View view){
+        updateTextView("-");
+    }
+
+    /**
+     * @param view
+     */
+    public void buttonOperatorMul(View view) {
         startNewExpression(view);
-        updateTextView("*");}
-    public void buttonOperatorDiv(View view){
+        updateTextView("*");
+    }
+
+    /**
+     * @param view
+     */
+    public void buttonOperatorDiv(View view) {
         startNewExpression(view);
-        updateTextView("/");}
-    public void buttonPlusOrMinus(View view){
+        updateTextView("/");
+    }
+
+    /**
+     * @param view
+     */
+    public void buttonPlusOrMinus(View view) {
         int temp;
-        if ( plusOrMinusCounter % 2 == 0 ){
+        if (plusOrMinusCounter % 2 == 0) {
             temp = Integer.parseInt(displayValue);
-            if ( temp < 0 ){
+            if (temp < 0) {
                 temp = temp * -1;
             }
-            displayValue = ""+ temp;
+            displayValue = "" + temp;
             textInputView.setText(displayValue);
 
-        }else {
+        } else {
             temp = Integer.parseInt(displayValue) * -1;
-            displayValue = ""+ temp;
+            displayValue = "" + temp;
             textInputView.setText(displayValue);
         }
         plusOrMinusCounter++;
     }
-    public void buttonModulus (View view){
+
+    /**
+     * @param view
+     */
+    public void buttonModulus(View view) {
 
         startNewExpression(view);
         updateTextView("%");
     }
 
     /**
-     * Need further actions.
      * @param view
      */
-    public void buttonPi (View view){
+    public void buttonBIN(View view) {
+    }
+
+    /**
+     * @param view
+     */
+    public void buttonHEX(View view) {
+    }
+
+    /**
+     * @param view
+     */
+    public void buttonDEC(View view) {
+    }
+
+    /**
+     * @param view
+     */
+    public void buttonSquare(View view) {
+    }
+
+    /**
+     * @param view
+     */
+    public void buttonSquareRoot(View view) {
+    }
+
+    /**
+     * @param view
+     */
+    public void buttonLogicGateXOR(View view) {
+    }
+
+    /**
+     * @param view
+     */
+    public void buttonLOG(View view) {
+    }
+
+    /**
+     * @param view
+     */
+    public void buttonTAN(View view) {
+    }
+
+    /**
+     * @param view
+     */
+    public void buttonLogicGateNOT(View view) {
+    }
+
+    /**
+     * @param view
+     */
+    public void buttonSIN(View view) {
+    }
+
+    /**
+     * @param view
+     */
+    public void buttonLogicGateOR(View view) {
+    }
+
+    /**
+     * @param view
+     */
+    public void buttonCOS(View view) {
+    }
+
+    /**
+     * @param view
+     */
+    public void buttonLogicGateAND(View view) {
+    }
+
+    /**
+     * @param view
+     */
+    public void buttonOpenBracket(View view) {
+    }
+
+    /**
+     * @param view
+     */
+    public void buttonClosedBracket(View view) {
+    }
+
+    /**
+     * Need further actions.
+     *
+     * @param view
+     */
+    public void buttonPi(View view) {
         textInputView.setText("π");
         textOutputView.setText("" + Math.PI);
 
@@ -146,9 +317,10 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * Do the calculate, need some optimize
+     *
      * @param view
      */
-    public void Calculate(View view){
+    public void Calculate(View view) {
         String inputExpression = textInputView.getText().toString();
         EquationtoPostfix PostfixExpression = new EquationtoPostfix(inputExpression);
         String result = PostfixExpression.toString();
@@ -156,12 +328,11 @@ public class MainActivity extends AppCompatActivity {
         try {
             Expressions finalExpressionTree = Expressions.parse(evaluate);
             textOutputView.setText(String.valueOf(finalExpressionTree.evaluate()));
-        }catch (ParseException e){
+            displayValue =  displayValue + String.valueOf(finalExpressionTree.evaluate());
+        } catch (ParseException e) {
 
-        }
-
-        finally {
-            equalClicked =true;
+        } finally {
+            equalClicked = true;
             //clearData(view);
         }
     }
